251d1c784eb76b4e391de80ef68f168d71c98479
Randle Kehinde Olanrewaju <kennyrandle09@hotmail.co.uk>
2019-12-20 12:46:46 +0000

    stored info

--- src/app/second-page/second-page.component.ts
+++ src/app/second-page/second-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //import the activated route and inject it to the bclass below
 import { ActivatedRoute } from '@angular/router';
+//store service import
+import { PdataserviceService } from '../pdataservice.service';
 
 @Component({
   selector: 'app-second-page',
@@ -11,7 +13,14 @@ export class SecondPageComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private data: PdataserviceService,
   ) { }
+
+  addToStoredData(information){
+    window.alert('Information stored')
+    this.data.addToSToredData(information)
+
+  }
 
   ngOnInit() {
     // this.route.paramMap.subscribe( )
