caa3baac3d7dbc5ba9c0a02f54bfc9c2f5578da5
Randle Kehinde Olanrewaju <kennyrandle09@hotmail.co.uk>
2020-01-08 14:17:10 +0000

    added dummy json data and DI in services

--- src/app/pdataservice.service.ts
+++ src/app/pdataservice.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PdataserviceService {
   storedData=[]
+  
+  constructor(private http: HttpClient) { }
 
   addToSToredData(information){
     this.storedData.push(information);
@@ -22,5 +25,8 @@ export class PdataserviceService {
 
   }
 
-  constructor() { }
+  getJsonInfo(){
+    return this.http.get('../assets/importantData.json')
+  }
+
 }
